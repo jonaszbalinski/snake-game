48aa8f85576369c82a977c4c884e6a1deded2bd7
Jonasz Balinski <->
2022-03-30 17:38:55 +0200

    Add project files.

---
+++ Snake/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+
+        GameSnake gameSnake;
+        public Form1()
+        {
+            InitializeComponent();
+            gameSnake = new GameSnake(20, 15);
+            DrawGame();
+        }
+
+        private void DrawGame()
+        {
+            pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics g = Graphics.FromImage(pictureBox.Image);
+            //g.Clear(Color.Gray);
+
+            for(int x = 0; x < gameSnake.Width; x++)
+            {
+                for(int y = 0; y < gameSnake.Height; y++)
+                {
+                    Color penColor = Color.DarkGray;
+                    if ((x + y) % 2 == 0) penColor = Color.Gray;
+
+                    g.FillRectangle(new SolidBrush(penColor), 
+                        new Rectangle(pictureBox.Width/gameSnake.Width * x,
+                                      pictureBox.Height / gameSnake.Height * y, 
+                                      pictureBox.Width / gameSnake.Width, 
+                                      pictureBox.Height / gameSnake.Height));
+                }
+            }
+
+            foreach(Point p in gameSnake.Snake)
+            {
+
+            }
+        }
+    }
+}
---
+++ Snake/GameSnake.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    class GameSnake
+    {
+        public enum DirectionsEnum {Up, Down, Left, Right};
+        private int width;
+        private int height;
+        private List<Point> snake;
+        private DirectionsEnum direction;
+        private Timer gameTimer;
+        public GameSnake(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            snake = new List<Point>();
+            snake.Add(new Point(this.width/2, this.height-3));
+            snake.Add(new Point(this.width/2, this.height-2));
+            snake.Add(new Point(this.width/2, this.height-1));
+
+            direction = DirectionsEnum.Up;
+
+            gameTimer = new Timer();
+            gameTimer.Interval = 500;
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Start();
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        public int Width { get => width;}
+        public int Height { get => height;}
+        public List<Point> Snake { get => snake;}
+        internal DirectionsEnum Direction { get => direction;}
+    }
+}
